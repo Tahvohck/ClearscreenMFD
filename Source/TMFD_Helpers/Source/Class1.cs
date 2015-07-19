12eb563610368a5fce619f2cf174385de1f888de
Tahvohck <Tahvohck@gmail.com>
2015-07-19 16:06:09 -0400

    DSDUMP breaks into 60-long chunks instead of 40-long now

--- Source/TMFD_Helpers/Source/Class1.cs
+++ Source/TMFD_Helpers/Source/Class1.cs
@@ -173,7 +173,7 @@ namespace TAHV_MFD
 				// Debug dump
 				case "DSDUMP":
 					string tmp = "", DSDUMP = DeepSettings.ToString();
-					for (int i = 0; i < DSDUMP.Length; i += 40) { tmp += DSDUMP.Substring(i, Math.Min(40, DSDUMP.Length - i)) + Environment.NewLine; }
+					for (int i = 0; i < DSDUMP.Length; i += 40) { tmp += DSDUMP.Substring(i, Math.Min(60, DSDUMP.Length - i)) + Environment.NewLine; }
 					tmp = tmp.Replace("[#", "[[]#");
 					return tmp;
 				// Various colors
