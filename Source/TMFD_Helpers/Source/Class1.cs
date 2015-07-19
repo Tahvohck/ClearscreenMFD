Tahvohck/ClearscreenMFD

commit 12eb563610368a5fce619f2cf174385de1f888de

DSDUMP breaks into 60-long chunks instead of 40-long now

Source/TMFD_Helpers/Source/Class1.cs
@@ -173,7 +173,7 @@ namespace TAHV_MFD
 				// Debug dump
 				case "DSDUMP":
 					string tmp = "", DSDUMP = DeepSettings.ToString();
-					for (int i = 0; i < DSDUMP.Length; i += 40) { tmp += DSDUMP.Substring(i, Math.Min(40, DSDUMP.Length - i)) + Environment.NewLine; }
+					for (int i = 0; i < DSDUMP.Length; i += 40) { tmp += DSDUMP.Substring(i, Math.Min(60, DSDUMP.Length - i)) + Environment.NewLine; }
 					tmp = tmp.Replace("[#", "[[]#");
 					return tmp;
 				// Various colors

commit 374300ef41fe1cfed1e439bfc2fc775e8c2e530a

Fix for bug introduced in 12eb5636: didn't chop string at proper lengths

Source/TMFD_Helpers/Source/Class1.cs
@@ -177,7 +177,7 @@ namespace TAHV_MFD
 				// Debug dump
 				case "DSDUMP":
 					string tmp = "", DSDUMP = DeepSettings.ToString();
-					for (int i = 0; i < DSDUMP.Length; i += 40) { tmp += DSDUMP.Substring(i, Math.Min(60, DSDUMP.Length - i)) + Environment.NewLine; }
+					for (int i = 0; i < DSDUMP.Length; i += 60) { tmp += DSDUMP.Substring(i, Math.Min(60, DSDUMP.Length - i)) + Environment.NewLine; }
 					tmp = tmp.Replace("[#", "[[]#");
 					return tmp;
 				// Various colors